3bc65e19c5cc65aea6435a0d31c57c96fbd040d0
complikatyed <katye.russell@gmail.com>
2016-09-24 12:49:39 -0500

    Add first two tests, install new versions of references

---
+++ SavingVariables.Tests/DAL/VariablesRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavingVariables.DAL;
+
+
+namespace SavingVariables.Tests.DAL
+{
+    [TestClass]
+    public class VariablesRepositoryTests
+    {
+
+        [TestMethod]
+        public void EnsureCanCreateRepoInstance()
+        {
+            VariablesRepository repo = new VariablesRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void EnsureRepoHasContext()
+        {
+            VariablesRepository repo = new VariablesRepository();
+
+            VariablesContext actual_context = repo.Context;
+
+            Assert.IsInstanceOfType(actual_context, typeof(VariablesContext));
+        }
+
+    }
+}
---
+++ SavingVariables/DAL/VariablesRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.DAL
+{
+    public class VariablesRepository
+    {
+        public VariablesContext Context { get; set; }
+
+        public VariablesRepository()
+        {
+            Context = new VariablesContext();
+        }
+
+        public VariablesRepository(VariablesContext _context)
+        {
+            Context = _context;
+        }
+
+    }
+}
