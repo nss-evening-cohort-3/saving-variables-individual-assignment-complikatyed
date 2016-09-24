66f5f3c7a84e1a73214584eec469ccf7d1746f99
complikatyed <katye.russell@gmail.com>
2016-09-24 14:47:16 -0500

    Add variables model in preparation for testing repo contents

---
+++ SavingVariables/Models/Variable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SavingVariables.Models
+{
+    public class Variable
+    {
+        [Key]
+        public int VariableId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(length: 1, ErrorMessage = "Choose one character, please.")]
+        public int Value { get; set; }
+    }
+}
